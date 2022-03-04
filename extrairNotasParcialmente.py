7eb9f872dd9254df67b5a74ae97f0090c68a4a4e
lhssluiz <lzhenriquesa@gmail.com>
2022-03-04 17:43:12 -0300

    Separação de arquivos extraiNotasCompleto: É o programa padrão que extrai as notas totais do arquivo MyClippings extraiNotasParcialmente: Ainda não foi modificado. Seria um programa para verificar as notas que já foram extraídas e a partir daí, só extrair o que falta

---
+++ extrairNotasParcialmente.py
@@ -0,0 +1,27 @@
+#Programa onde eu posso passar capítulo por capítulo de um livro
+# Abre um JSON
+# Busca e gera as notas por completo
+# Gera um dict do JSON das notas já existentes (extraídas anteriormente)
+# Compara as notas e adiciona as novas (que não existiam nesse arquivo anterior)
+# Organiza o novo dicionario
+# Salva em JSON ou atualiza o anterior
+# Gera um novo WORD
+## Essa solução acima, é para o caso de ter que copiar e colar
+## Todas as minhas notas manuais feitas anteriormente
+
+# Uma possibilidade onde eu não precisa copiar e colar manualmente
+# pode ser encontrada nessa referência https://stackoverflow.com/questions/61147974/read-from-a-word-file-in-python
+# Eu precisava extrair tudo do word atual, passar para um dict, criando chaves em ordem para manter a "ordem"
+# depois fazer o processo de inserção de novas novas
+# e por fim gerar o novo arquivo, já com as notas
+
+
+
+from operator import truediv
+import extraiNotasCompleto
+
+def openJsonFile():
+    return jsonFile
+
+def compareJsonFile():
+    return true
